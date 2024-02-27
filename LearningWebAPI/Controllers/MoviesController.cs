6d5c0fecf514ecbacaa2b7825795efd518fe5273
Aji Suryawan <kazumovie132@gmail.com>
2024-02-27 14:37:19 +0700

    Add project files.

---
+++ LearningWebAPI/Controllers/MoviesController.cs
@@ -0,0 +1,106 @@
+﻿using LearningWebAPI.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace LearningWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+        private readonly MovieContext _dbContext;
+        public MoviesController(MovieContext dbContext) { 
+            _dbContext = dbContext;
+        }
+
+        // Get: api/getMovies
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Movie>>> GetMovies() { 
+            if (_dbContext == null)
+            {
+                return NotFound();
+            }
+            return await _dbContext.Movies.ToListAsync();
+        }
+
+        // Get: api/Movies
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Movie>> GetMovie(int id)
+        {
+            if (_dbContext == null)
+            {
+                return NotFound();
+            }
+
+            var movie = await _dbContext.Movies.FindAsync(id);
+            if (movie == null)
+            {
+                return NotFound();
+            }
+            return movie;
+        }
+
+        // Post: api/Movies
+        [HttpPost]
+        public async Task<ActionResult<Movie>> PostMovie(Movie movie)
+        {
+            _dbContext.Movies.Add(movie);
+            await _dbContext.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetMovie) , new {id = movie.Id}, movie);
+        }
+
+        // PUT: api/Movies/5
+        [HttpPut("{id}")]
+        public async Task<ActionResult> PutMovies(int id, Movie movie)
+        {
+            if(id != movie.Id)
+            {
+                return BadRequest();
+            }
+            _dbContext.Entry(movie).State = EntityState.Modified;
+            try
+            {
+                await _dbContext.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!MovieExist(id))
+                {
+                    return NotFound();
+                }
+                else {
+                    throw;
+                }
+            }
+            return NoContent();
+        }
+
+        private bool MovieExist(long id) {
+            return (_dbContext.Movies?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+
+        // DELETE: api/Movies/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteMovies(int id)
+        {
+            if (_dbContext.Movies == null)
+            {
+                return NotFound();
+            }
+            
+            var movie = await _dbContext.Movies.FindAsync(id);
+            if (movie == null)
+            {
+                return NotFound();
+            }
+            _dbContext.Movies.Remove(movie);
+            await _dbContext.SaveChangesAsync();
+            return NoContent();
+        }
+
+
+
+    }
+}
---
+++ LearningWebAPI/Controllers/UserController.cs
@@ -0,0 +1,75 @@
+﻿using LearningWebAPI.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Diagnostics;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace LearningWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : Controller
+    {
+        private readonly MovieContext _dbContext;
+        private readonly IConfiguration _config;
+        public UserController(MovieContext dbContext, IConfiguration config)
+        {
+            _dbContext = dbContext;
+            _config = config;
+        }
+
+        // To generate token
+        private string GenerateToken(UserModel user)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+            Debug.WriteLine("jwt key : " +_config["Jwt:Key"]);
+            Debug.WriteLine("jwt Issuer : " + _config["Jwt:Issuer"]);
+            Debug.WriteLine("jwt Audience : " + _config["Jwt:Audience"]);
+            Debug.WriteLine("data user " + user.Username.ToString());
+            Debug.WriteLine("data user " + user.Role.ToString());
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier,user.Username),
+                new Claim(ClaimTypes.Role,user.Role)
+            };
+            var token = new JwtSecurityToken(_config["Jwt:Issuer"],
+                _config["Jwt:Audience"],
+                claims,
+                expires: DateTime.Now.AddMinutes(30),
+                signingCredentials: credentials);
+
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<UserModel>> Login(UserModel userModel)
+        {
+            if (_dbContext == null)
+            {
+                return NotFound();
+            }
+
+            var user = await _dbContext.Users.Where(m => m.Username == userModel.Username && m.Password == userModel.Password)
+    .FirstOrDefaultAsync();
+            if (user == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                var token = GenerateToken(user);
+                //return Ok(token);
+                return Ok(new { response = token });
+            }
+            return user;
+        }
+
+    }
+}
